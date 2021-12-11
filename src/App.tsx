435dfc81befd7a6f41da0597f47871c4e2896da5
Сушков Андрей Викторович <asushkov@phoenixit.ru>
2021-12-11 13:40:04 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Router} from 'react-router-dom';
+import {history} from "./history";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface Props {
+
 }
 
-export default App;
+export const App: React.FC<Props> = () => {
+  return (
+    <Router history={history}>
+      <div>
+        <button className="waves-effect deep-purple lighten-4 btn-large">Button</button>
+      </div>
+    </Router>
+  );
+}
