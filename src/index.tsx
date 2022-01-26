bb1b3161486f0f8d4a5d4ed1320336881daec850
Андрей Сушков <andrejsuskov@MacBook-Air-Andrej.local>
2022-01-27 00:40:40 +0300

    добавил redux persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from "./App";
 import {Provider} from "react-redux";
-import {store} from "./store/store";
+import {persistor, store} from "./store/store";
 import {Router} from "react-router-dom";
 import {history} from "./history";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Router history={history}>
     <Provider store={store}>
-      <App/>
+      <PersistGate persistor={persistor}>
+        <App/>
+      </PersistGate>
     </Provider>
   </Router>,
   document.getElementById('root')
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,20 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore, createStore} from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import {rootReducer} from './root';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
 });
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
