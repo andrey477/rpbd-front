2d4f55c7546b567c3b90eff5d8483e4a956c123b
Андрей Сушков <andrejsuskov@MacBook-Air-Andrej.local>
2022-02-23 15:51:23 +0300

    добавил моки

--- src/mock/competitionMock.ts
+++ src/mock/competitionMock.ts
@@ -38,6 +38,149 @@ export const competitionMock: Competition.BodyCompetition[] = [
 					model: '5 series'
 				}
 			},
+			{
+				number: '22',
+				person: {
+					id: "419439f1-8a70-45f8-adf7-e9194c8ab52c",
+					name: "Иван",
+					surname: "Иванов",
+					city: "Рязань",
+					email: "iivanov@mail.com",
+					age: 31,
+				},
+				status: Status.REJECTED,
+				race: {
+					attempts: 2,
+					rating: {
+						stage_1: [1, 2, 3],
+						stage_2: [1, 2, 3],
+						stage_3: [1, 2, 3],
+					}
+				},
+				vehicle: {
+					id: 'gfgfd',
+					horsepower: 2000,
+					brand: 'bmw',
+					model: '5 series'
+				}
+			},
+			{
+				number: '23',
+				person: {
+					id: "419439f1-8a70-45f8-adf7-e9194c8ab52c",
+					name: "Иван",
+					surname: "Иванов",
+					city: "Рязань",
+					email: "iivanov@mail.com",
+					age: 31,
+				},
+				status: Status.SENT,
+				race: {
+					attempts: 2,
+					rating: {
+						stage_1: [1, 2, 3],
+						stage_2: [1, 2, 3],
+						stage_3: [1, 2, 3],
+					}
+				},
+				vehicle: {
+					id: 'gfgfd',
+					horsepower: 2000,
+					brand: 'bmw',
+					model: '5 series'
+				}
+			},
+		],
+		stage: Stage.RECORD,
+		total: [],
+	},
+	{
+		id: 'jfsdnjfnsjdfnjnfsjn',
+		name: 'Тестовое соревнование',
+		attempts: 2,
+		isOpen: true,
+		race_1: [],
+		race_2: [],
+		race_3: [],
+		racers: [
+			{
+				number: '17',
+				person: {
+					id: "419439f1-8a70-45f8-adf7-e9194c8ab52c",
+					name: "Иван",
+					surname: "Иванов",
+					city: "Рязань",
+					email: "iivanov@mail.com",
+					age: 31,
+				},
+				status: Status.APPROVED,
+				race: {
+					attempts: 2,
+					rating: {
+						stage_1: [1, 2, 3],
+						stage_2: [1, 2, 3],
+						stage_3: [1, 2, 3],
+					}
+				},
+				vehicle: {
+					id: 'gfgfd',
+					horsepower: 2000,
+					brand: 'bmw',
+					model: '5 series'
+				}
+			},
+			{
+				number: '22',
+				person: {
+					id: "419439f1-8a70-45f8-adf7-e9194c8ab52c",
+					name: "Иван",
+					surname: "Иванов",
+					city: "Рязань",
+					email: "iivanov@mail.com",
+					age: 31,
+				},
+				status: Status.REJECTED,
+				race: {
+					attempts: 2,
+					rating: {
+						stage_1: [1, 2, 3],
+						stage_2: [1, 2, 3],
+						stage_3: [1, 2, 3],
+					}
+				},
+				vehicle: {
+					id: 'gfgfd',
+					horsepower: 2000,
+					brand: 'bmw',
+					model: '5 series'
+				}
+			},
+			{
+				number: '23',
+				person: {
+					id: "419439f1-8a70-45f8-adf7-e9194c8ab52c",
+					name: "Иван",
+					surname: "Иванов",
+					city: "Рязань",
+					email: "iivanov@mail.com",
+					age: 31,
+				},
+				status: Status.SENT,
+				race: {
+					attempts: 2,
+					rating: {
+						stage_1: [1, 2, 3],
+						stage_2: [1, 2, 3],
+						stage_3: [1, 2, 3],
+					}
+				},
+				vehicle: {
+					id: 'gfgfd',
+					horsepower: 2000,
+					brand: 'bmw',
+					model: '5 series'
+				}
+			},
 		],
 		stage: Stage.RECORD,
 		total: [],
